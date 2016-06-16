cfc1e48fa1443c162072ad90b1eaf5fd09732ddd
DennisSkoko <dennis.skoko@hotmail.com>
2016-06-16 16:00:39 +0200

    Fixed HTML error on services page

--- app/Resources/Views/pages/services.view.php
+++ app/Resources/Views/pages/services.view.php
@@ -1,29 +1,22 @@
 <?php use DS\Utilities\Url; ?>
 <div class="container">
-    <ul class="row services-all">
-        <div class="col-lg-8 col-lg-push-2">
+    <div class="col-md-6 col-md-push-3">
+        <ul class="row services-all">
 
-            <li class="col-sm-4">
-                <a href="<?= Url::make("service/weather") ?>">
-                    <span class="glyphicon glyphicon-cloud"></span>
-                    <h3>Weather</h3>
-                </a>
-            </li>
-
-            <li class="col-sm-4">
+            <li class="col-sm-6">
                 <a href="<?= Url::make("service/ipinfo") ?>">
                     <span class="glyphicon glyphicon-info-sign"></span>
                     <h3>IP Info</h3>
                 </a>
             </li>
 
-            <li class="col-sm-4">
+            <li class="col-sm-6">
                 <a href="<?= Url::make("service/calendar") ?>">
                     <span class="glyphicon glyphicon-calendar"></span>
                     <h3>Calendar</h3>
                 </a>
             </li>
 
-        </div>
-    </ul>
+        </ul>
+    </div>
 </div>
