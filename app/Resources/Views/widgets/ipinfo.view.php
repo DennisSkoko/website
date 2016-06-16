ddbe799feed3a2fd7752d708600374d3f88b8843
DennisSkoko <dennis.skoko@hotmail.com>
2016-06-16 16:11:33 +0200

    Added alt attribute in img and fixed HTML errors

--- app/Resources/Views/widgets/ipinfo.view.php
+++ app/Resources/Views/widgets/ipinfo.view.php
@@ -1,6 +1,10 @@
 <div class="ipinfo text-center">
     <h2><?= $ipinfo->getIp() ?></h2>
-    <img style="border-radius: 50%" src="https://maps.googleapis.com/maps/api/staticmap?center=<?= $ipinfo->getLoc() ?>&zoom=10&size=200x200&sensor=false">
+    <img
+        style="border-radius: 50%"
+        src="https://maps.googleapis.com/maps/api/staticmap?center=<?= $ipinfo->getLoc() ?>&amp;zoom=10&amp;size=200x200&amp;sensor=false"
+        alt="A map from Google with the following coordinates: <?= $ipinfo->getLoc() ?>"
+    >
 
     <h2>
         <?= $ipinfo->getCity() !== null ? $ipinfo->getCity() : "City not available" ?><?= $ipinfo->getCountry() !== null ? ", " . $ipinfo->getCountry() : null ?>
