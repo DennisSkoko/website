7099b7efd56d57d2d784548ae9696d2e0de6c3f8
DennisSkoko <dennis.skoko@hotmail.com>
2016-06-17 22:42:47 +0200

    Updated IP:class and added description in PHPDoc

--- app/Source/IP/IP.php
+++ app/Source/IP/IP.php
@@ -5,21 +5,29 @@ namespace DS\IP;
 /**
  * Class IP
  *
+ * Used to get the IP address.
+ *
  * @package DS\IP
  * @author  Dennis Skoko
  */
 class IP
 {
     /**
-     * Will get the IP address. If it is 192.168.1.1 then will call ipinfo to get the real ip address.
+     * Will get the IP address. If it is false IP then will call ipinfo to get the real ip address.
      *
      * @return string
      */
     public static function get()
     {
+        $falseIPs = [
+            "192.168.1.1",
+            "127.0.0.1",
+            "::1"
+        ];
+
         $ip = $_SERVER["REMOTE_ADDR"];
 
-        if ($ip == "192.168.1.1") {
+        if (in_array($ip, $falseIPs)) {
             $ip = trim(file_get_contents("http://ipinfo.io/ip"));
         }
 
