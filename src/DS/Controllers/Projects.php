31613b57d6c4a3a6df6c304181fa5d98798c4e4f
Dennis Skoko <dennis.skoko@hotmail.com>
2016-12-29 22:58:17 +0100

    Updated alert text in projects page2

--- src/DS/Controllers/Projects.php
+++ src/DS/Controllers/Projects.php
@@ -16,7 +16,7 @@ class Projects extends Controller
      */
     public function all(Request $request, Response $response)
     {
-        $this->container->session->flash('info', 'This site is being implemented. Please come back in a few days.');
+        $this->container->session->flash('info', 'This page is being implemented. Please come back in a few days.');
 
         return $this->container->theme
             ->with([
