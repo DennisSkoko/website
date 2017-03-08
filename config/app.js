8fb1fda0050f06f29243e5abf9b049331b108a37
Dennis Skoko <dennis.skoko@hotmail.com>
2017-03-08 21:52:47 +0100

    Added base files

---
+++ config/app.js
@@ -0,0 +1,16 @@
+/*
+ * Configuration for the application.
+ */
+'use strict';
+
+module.exports = {
+
+    port: 80,
+    publicDir: 'public',
+
+    view: {
+        folder: 'res/views',
+        engine: 'pug'
+    }
+
+};
---
+++ config/theme.js
@@ -0,0 +1,29 @@
+/*
+ * Configuration for the theme of this application.
+ */
+'use strict';
+
+module.exports = {
+
+    title: 'Dennis Skoko',
+    charset: 'utf-8',
+
+    nav: {
+        links: [
+            {
+                path: '/',
+                text: 'Home'
+            }
+        ]
+    },
+
+    stylesheets: [
+        'css/bootstrap.css'
+    ],
+
+    javascript: [
+        'js/jquery.js',
+        'js/bootstrap.js'
+    ]
+
+};
