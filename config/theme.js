d34ddfa8ec58c0688fa1745d941fc3d5337ed326
Dennis Skoko <dennis.skoko@hotmail.com>
2017-03-14 23:57:53 +0100

    Updated jquery

--- config/theme.js
+++ config/theme.js
@@ -30,7 +30,7 @@ module.exports = {
   ],
 
   javascript: [
-    'js/jquery.js',
+    'https://code.jquery.com/jquery-3.1.1.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
   ]
 };
