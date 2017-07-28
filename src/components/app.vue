126a959f312b5d0ce4f095a5b7fa32acbe369f4c
Dennis Skoko <dennis.skoko@hotmail.com>
2017-07-28 22:18:47 +0200

    Added vuetify

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,4 +1,7 @@
 <template lang="pug">
-  div
-    h1 Dennis Skoko
+  v-app(dark)
 </template>
+
+<style lang="stylus">
+@import '../../node_modules/vuetify/src/stylus/main.styl'
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 'use strict'
 
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 
 import App from './components/app.vue'
+
+Vue.use(Vuetify)
 
 new Vue({ // eslint-disable-line no-new
   el: '#ds',
