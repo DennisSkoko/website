e4f226aee914c761f1ce9f94e2cd84ace9506672
Dennis Skoko <dennis.skoko@hotmail.com>
2017-07-29 14:32:00 +0200

    Added navigation

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 'use strict'
 
 import About from './components/about.vue'
+import Projects from './components/projects.vue'
 import Contact from './components/contact.vue'
 
 export default [
   { path: '/', name: 'about', component: About },
+  { path: '/projects', name: 'projects', component: Projects },
   { path: '/contact', name: 'contact', component: Contact }
 ]
