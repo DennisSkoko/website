dc62095a0947a1f8d1a7263ccc45437a4b0e8551
Dennis Skoko <dennis.skoko@hotmail.com>
2017-07-30 11:44:53 +0200

    Added form for contact page

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,3 +1,52 @@
-<template>
-  <h1>Contact</h1>
+<template lang="pug">
+  v-container
+    v-layout(justify-center)
+      v-flex(xs12, md8)
+        form.grey.darken-3.pa-3.mt-4.elevation-3(v-on:submit.prevent='submit()')
+          .text-xs-center
+            h3.white--text Contact
+          v-text-field(label='Email', v-model='email', required)
+
+          v-text-field(
+            label='Subject',
+            counter, max='25',
+            maxlength='25',
+            v-model='subject',
+            required
+          )
+
+          v-text-field(
+            label='Message',
+            hint='Supports markdown',
+            textarea,
+            multi-line,
+            counter,
+            max='250',
+            maxlength='250'
+            v-model='message',
+            required
+          )
+          v-btn(block, outline, type='submit') Submit
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      subject: '',
+      message: ''
+    }
+  },
+  methods: {
+    submit () {
+      console.log('Submitted')
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.input-group--multi-line textarea
+  resize: none
+</style>
