10eed2b455c15a9f8669f54af9b161bd53c99c49
Dennis Skoko <dennis.skoko@hotmail.com>
2017-07-30 14:24:31 +0200

    Added projects page

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,3 +1,39 @@
-<template>
-  <h1>Projects</h1>
+<template lang="pug">
+  v-container
+    v-layout(wrap)
+      v-flex(xs12, md6, v-for='project in projects', :key='project.id')
+        v-card.my-3
+          v-card-media
+            v-carousel(light)
+              v-carousel-item(
+                v-for='img in project.images',
+                :key='img',
+                :src='"img/projects/" + project.id + "/" + img'
+              )
+          v-card-title.pa-3
+            div
+              h4.white--text.mb-0 {{ project.title }}
+              .tags(v-if='project.tags')
+                v-chip.ma-1(v-for='tag in project.tags.split(",")', :key='tag', small) {{ tag }}
+          v-card-text.pa-3
+            p.mb-0 {{ project.description }}
+          v-card-actions
+            v-btn(flat, block, :href='project.website', target='_blank') Go to project
 </template>
+
+<script>
+import projects from '../../res/projects.json'
+
+export default {
+  data () {
+    return {
+      projects: projects
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.carousel
+  height: 300px
+</style>
