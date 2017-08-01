6345a46f4ad5c986964a24f0b87b25c2b131e2a7
Dennis Skoko <dennis.skoko@hotmail.com>
2017-08-01 17:09:58 +0200

    Added 404-page

---
+++ src/components/not-found.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div
+    .jumbotron.grey.lighten-3.pa-4
+      v-container
+        h1 404 Not Found
+        p.headline.
+          I'm sorry but I couldn't found what you were looking for.
+          #[router-link(:to='{ name: "about" }') Click here] to return to the home page.
+</template>
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@
 import About from './components/about.vue'
 import Projects from './components/projects.vue'
 import Contact from './components/contact.vue'
+import NotFound from './components/not-found.vue'
 
 export default [
   { path: '/', name: 'about', component: About },
   { path: '/projects', name: 'projects', component: Projects },
-  { path: '/contact', name: 'contact', component: Contact }
+  { path: '/contact', name: 'contact', component: Contact },
+  { path: '*', name: 'not-found', component: NotFound }
 ]
