a54f0c79c70f80759ed242d37cdec4370610612b
Dennis Skoko <dennis.skoko@hotmail.com>
2018-03-07 20:43:35 +0100

    Added navbar and updated router config

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,31 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    nav.navbar.navbar-expand-lg.navbar-light.bg-light
+      .container
+        span.navbar-brand.mb-0.h1 Dennis Skoko
+        button.navbar-toggler(data-toggle='collapse' data-target='#navbarNav')
+          span.navbar-toggler-icon
+        #navbarNav.collapse.navbar-collapse
+          .mx-auto
+          ul.navbar-nav
+            router-link.nav-item(tag='li', v-for='(link, i) in links', :key='i', :to='link.to')
+              a.nav-link {{ link.text }}
+
+    router-view
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      links: [
+        { text: 'Home', to: '/home' },
+        { text: 'Portfolio', to: '/portfolio' },
+        { text: 'Contact', to: '/contact' }
+      ]
+    }
+  }
 }
 </script>
 
