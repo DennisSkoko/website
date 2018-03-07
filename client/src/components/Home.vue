b16b92d97f3c0835bc40d4c1a3ae7a741de515c5
Dennis Skoko <dennis.skoko@hotmail.com>
2018-03-07 21:24:06 +0100

    Updated home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,18 @@
-<template>
-  <div class="hello"></div>
+<template lang="pug">
+  .home
+    .jumbotron.jumbotron-fluid
+      .container
+        h1.display-1 Welcome!
+        p.lead.
+          Hi there, in here you can find basic information about me and a way to
+          contact me. If you want to see what I'm working on at home you can
+          head over to Github. If you want more information on my education and
+          experience then take a look at my LinkedIn page.
+
+        p.mb-0
+          a.mx-1.btn.btn-outline-dark.btn-lg(href='https://github.com/DennisSkoko', target='_blank') Github
+          a.mx-1.btn.btn-outline-dark.btn-lg(href='https://www.linkedin.com/in/dennis-skoko-a99418128/', target='_blank') LinkedIn
+          router-link.mx-1.btn.btn-outline-dark.btn-lg(to='/portfolio') Portfolio
 </template>
 
 <script>
