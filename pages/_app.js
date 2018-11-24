28e9919ef8e9b7bfaa6a7af76918ecd10eeae0c3
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-24 19:34:31 +0100

    Moves the header to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import Header from '../components/Header'
+import HeaderTitle from '../components/HeaderTitle'
+import HeaderNav from '../components/HeaderNav'
+import HeaderNavLink from '../components/HeaderNavLink'
 import GlobalStyles from '../style/GlobalStyles'
 import ThemeProvider from '../style/ThemeProvider'
 
@@ -12,6 +16,16 @@ class MyApp extends App {
         <ThemeProvider>
           <>
             <GlobalStyles />
+
+            <Header>
+              <HeaderTitle>Dennis Skoko</HeaderTitle>
+              <HeaderNav>
+                <HeaderNavLink href='/'>Home</HeaderNavLink>
+                <HeaderNavLink href='/portfolio'>Portfolio</HeaderNavLink>
+                <HeaderNavLink href='/contact'>Contact</HeaderNavLink>
+              </HeaderNav>
+            </Header>
+
             <Component {...pageProps} />
           </>
         </ThemeProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import Container from '../components/Container'
-import Header from '../components/Header'
-import HeaderTitle from '../components/HeaderTitle'
 import Heading from '../components/Heading'
-import HeaderNav from '../components/HeaderNav'
-import HeaderNavLink from '../components/HeaderNavLink'
 
 function Index () {
   return (
     <>
-      <Header>
-        <HeaderTitle>Dennis Skoko</HeaderTitle>
-        <HeaderNav>
-          <HeaderNavLink href='/'>Home</HeaderNavLink>
-          <HeaderNavLink href='/portfolio'>Portfolio</HeaderNavLink>
-          <HeaderNavLink href='/contact'>Contact</HeaderNavLink>
-        </HeaderNav>
-      </Header>
       <Container>
         <main>
           <Heading>Welcome</Heading>
