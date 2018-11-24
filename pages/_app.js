ec5fb71b52bf75c6f87632422aa65154084a6012
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-24 17:27:28 +0100

    Resolves all lint issues

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import App, { Container } from 'next/app'
 
 class MyApp extends App {
-  render() {
+  render () {
     const { Component, pageProps } = this.props
 
     return (
