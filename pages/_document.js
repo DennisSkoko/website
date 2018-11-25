1255064de1a03247aaff9dba2b9a25ad88109b71
Dennis Skoko <dennis.skoko@hotmail.com>
2018-11-25 11:08:03 +0100

    Adds favicon

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,14 @@ class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name='msapplication-TileColor' content='#000000' />
+          <meta name='msapplication-config' content='/static/browserconfig.xml?v=3e8A78pMBA' />
+          <meta name='theme-color' content='#000000' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png?v=3e8A78pMBA' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png?v=3e8A78pMBA' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png?v=3e8A78pMBA' />
+          <link rel='manifest' href='/static/site.webmanifest?v=3e8A78pMBA' />
+          <link rel='shortcut icon' href='/static/favicon.ico?v=3e8A78pMBA' />
           {this.props.styleTags}
         </Head>
         <body>
