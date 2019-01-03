32ff5bac4ad69f672e2a645401941e383b8573b6
Dennis Skoko <dennis.skoko@hotmail.com>
2019-01-03 21:01:51 +0100

    Changes the on hover style for cards

--- ui/Card.js
+++ ui/Card.js
@@ -3,13 +3,14 @@ import { transitions } from 'polished'
 
 const Card = styled.div`
   padding: ${({ theme }) => `${theme.spacing.md}`};
-  border: 0.1875rem solid transparent;
   display: block;
   text-decoration: none;
-  ${({ theme }) => transitions(['border-color', theme.transition.normal])};
+  position: relative;
+  top: 0;
+  ${({ theme }) => transitions(['top'], theme.transition.normal)};
 
   &:hover {
-    border-color: ${({ theme }) => theme.color.primary.base};
+    top: ${({ theme }) => `-${theme.spacing.xs}`};
   }
 `
 
