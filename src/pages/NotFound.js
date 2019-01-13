3d1e226169b7fc8da87d85e6b39808988560610b
Dennis Skoko <dennis.skoko@hotmail.com>
2019-01-13 14:32:18 +0100

    Updates the NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Container from '../ui/Container'
+import Heading from '../ui/Heading'
+import Link from '../ui/Link'
+import Text from '../ui/Text'
 
 function NotFound () {
   return (
-    <h1>Not Found</h1>
+    <Container>
+      <Heading as='h2' marginTop centered>Page not found</Heading>
+      <Text centered>
+        The page you are looking for doesn't exists. <Link to='/'>Click here</Link>
+        {' '}to return to the homepage.
+      </Text>
+    </Container>
   )
 }
 
