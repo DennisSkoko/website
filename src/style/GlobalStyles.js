c94f58fde95e2700b04868c3b7a2221033e2d367
Dennis Skoko <dennis.skoko@knowit.se>
2019-06-01 21:38:45 +0200

    Moves the loading of fonts to html file

--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -4,8 +4,6 @@ import { normalize } from 'polished'
 const GlobalStyles = createGlobalStyle`
   ${normalize()}
 
-  @import url('https://fonts.googleapis.com/css?family=Caveat:400,700|Montserrat:400,400i,700');
-
   html {
     font-size: ${({ theme }) => theme.font.baseSize};
     background-color: ${({ theme }) => theme.color.white.base};
