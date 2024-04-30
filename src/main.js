512c813999a5f950fde9e2fc0d9e3d9c82919881
Dennis Skoko <dennis.skoko@hotmail.com>
2024-04-30 05:05:41 +0000

    Add simple tracking

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { create } from 'ackee-tracker'
 import 'normalize.css'
 import './global.css'
 import './journey'
@@ -7,3 +8,12 @@ document.querySelector('#app').innerHTML = `
   <app-jumbotron anchor="#journey"></app-jumbotron>
   <app-journey id="journey"></app-journey>
 `
+
+if (import.meta.env.PROD) {
+  ;(function() {
+    create('https://analytics.dennisskoko.com', {
+      ignoreOwnVisits: false,
+    })
+      .record('53cf9fd5-9d55-42d0-9a23-5f0ca1926074')
+  })()
+}
